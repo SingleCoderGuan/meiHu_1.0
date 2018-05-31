9e6d0bb028739f5daca719f30e7751ad3add1303
SingleCoderGuan <920758051@qq.com>
2018-05-31 19:51:08 +0800

    板块转换

--- src/main/java/meiHu/control/luntanControl.java
+++ src/main/java/meiHu/control/luntanControl.java
@@ -22,27 +22,50 @@ public class luntanControl {
     private LuntanService luntanService;
 
     @RequestMapping(value = "/luntanshouye.action")
-    public void showbankuai(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<ForumTopic> topicList = luntanService.getAllTopics();
-        request.setAttribute("topicList",topicList);
-
-        List<ForumPost> postList = luntanService.selectPostsByTid(1);
-        request.setAttribute("postList",postList);
+    public void luntanshouye(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String tid = request.getParameter("tid");
+        int tid1 = Integer.parseInt(tid);
+        if(tid1==1){
+            List<ForumTopic> topicList = luntanService.getAllTopics();
+            List<ForumPost> postList = luntanService.selectPostsByTid(1);
+            request.setAttribute("topicList",topicList);
+            request.setAttribute("postList",postList);
 //        System.out.println("postd长度"+postList.size());
-        request.getRequestDispatcher("/jsp/luntan.jsp").forward(request,response);
+            request.getRequestDispatcher("/jsp/luntan.jsp").forward(request,response);
+        }
+        else{
+            List<ForumPost> postList = luntanService.selectPostsByTid(tid1);
+            List<ForumTopic> topicList = luntanService.getAllTopics();
+            request.setAttribute("topicList",topicList);
+            request.setAttribute("postList",postList);
+            request.getRequestDispatcher("/jsp/luntan.jsp").forward(request,response);
+
+        }
+
+
+
+
     }
 
 
     @RequestMapping(value = "/tiezidetail.action",method = RequestMethod.GET)
     public void tiezidetail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pid = request.getParameter("pid");
-        System.out.println(pid);
+        //System.out.println(pid);
         int pid1 = Integer.parseInt(pid);
         ForumPost forumPost = luntanService.selectPostByPid(pid1);
         request.setAttribute("forumPost",forumPost);
-        System.out.println(forumPost.getPtitle());
+       // System.out.println(forumPost.getPtitle());
         request.getRequestDispatcher("/jsp/tiezidetail.jsp").forward(request,response);
     }
+
+
+   /* @RequestMapping(value = "/bankuai.action")
+    public void bankuai(HttpServletRequest request, HttpServletResponse response){
+        String tid = request.getParameter("tid");
+        int tid1 = Integer.parseInt(tid);
+
+    }*/
 
 
 }
